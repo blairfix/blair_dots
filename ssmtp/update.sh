305652aa3b892072d99af79029112098ba7ff132
blair <blair@blairfix@gmail.com>
2022-02-05 19:56:41 -0500

    added ssmtp

---
+++ ssmtp/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# prompt for gmail app password
+echo Enter gmail app password
+read pw
+
+# add password to file
+sed  "s|password|$pw|g" ssmtp.conf > ssmtp.tmp
+
+# install config file
+sudo cp ssmtp.tmp /etc/ssmtp/ssmtp.conf
+sudo chown root /etc/ssmtp/ssmtp.conf
+trash ssmtp.tmp
+
+# test email
+echo "Subject: testing ssmtp mail ... " | sendmail blairfix@gmail.com
+
+
+
