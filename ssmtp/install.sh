6ec213ce34f9a3975f1c0811b4f1d65c5c205ce9
Blair Fix <blairfix@gmail.com>
2022-02-13 08:40:30 -0500

    included hostname in ssmtp

--- ssmtp/install.sh
+++ ssmtp/install.sh
@@ -4,14 +4,18 @@
 echo Enter gmail app password
 read pw
 
+
 # add password to file
 sed  "s|password|$pw|g" ssmtp.conf > ssmtp.tmp
+
+# add hostname to file
+sed -i "s|comp_name|$HOSTNAME|g" ssmtp.tmp
 
 # install config file
 sudo mkdir /etc/ssmtp
 sudo cp ssmtp.tmp /etc/ssmtp/ssmtp.conf
 sudo chown root /etc/ssmtp/ssmtp.conf
-trash ssmtp.tmp
+rm ssmtp.tmp
 
 # test email
 #echo "Subject: testing ssmtp mail ... " | sendmail blairfix@gmail.com
