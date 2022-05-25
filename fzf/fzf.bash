3dba2013e509a6758d3d637960719e8c1df151bb
blairfix <blairfix@gmail.com>
2022-05-25 14:29:03 -0400

    fixed fzf

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -14,7 +14,7 @@ source "/home/blair/.fzf/shell/key-bindings.bash"
 
 
 # Set ripgrep as the default source for fzf
-export FZF_DEFAULT_COMMAND="rg --files --follow --exclude" 
+export FZF_DEFAULT_COMMAND="rg --files --follow" 
 
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
