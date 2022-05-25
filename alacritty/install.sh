f1cbb0de700bb6ffd13e37932d718e3fc991571f
blairfix <blairfix@gmail.com>
2022-05-25 10:32:08 -0400

    variable font for alacritty

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 mkdir  ~/.config/alacritty/
-
 cp alacritty.yml ~/.config/alacritty/
+
+# get current host
+host=$(hostname)
+
+# set font size
+
+if [ $host == 'blair-server' ]
+then
+   sed -i 's/font-size-here/11/g' ~/.config/alacritty/alacritty.yml
+fi
+
+if [ $host == 'blair-laptop' ]
+then
+   sed -i 's/font-size-here/10/g' ~/.config/alacritty/alacritty.yml
+fi
+
