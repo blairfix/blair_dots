5d10381e6a797009b33d089aa5e2b8709e332834
blairfix <blairfix@gmail.com>
2022-05-29 19:53:47 -0400

    updated grub

--- grub/install.sh
+++ grub/install.sh
@@ -6,6 +6,10 @@ sudo cp /etc/default/grub /etc/default/grub.bak
 # copy config
 sudo cp grub /etc/default/grub
 
+# change ownership
+sudo chown root /etc/default/grub
+sudo chgrp root /etc/default/grub
+
 # generate grub config file
 grub-mkconfig -o /boot/grub/grub.cfg
 
