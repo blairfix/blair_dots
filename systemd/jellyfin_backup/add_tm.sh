40e62dd4c169276c6e487a1d721daedf8e4397eb
blairfix <blairfix@gmail.com>
2023-04-16 09:23:17 -0400

    update

--- systemd/jellyfin_backup/add_tm.sh
+++ systemd/jellyfin_backup/add_tm.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 cp *.timer *.service /etc/systemd/system
 
 systemctl daemon-reload
-systemctl enable jellyfin_backup.timer
-systemctl start jellyfin_backup.timer
-systemctl restart jellyfin_backup.timer
-
+#systemctl enable jellyfin_backup.timer
+#systemctl start jellyfin_backup.timer
+#systemctl restart jellyfin_backup.timer
 
 systemctl enable jellyfin_restart.timer
 systemctl start jellyfin_restart.timer
